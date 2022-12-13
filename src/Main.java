e57064ecfa77f977b220e1017c6a9b7c7e1e9dad
Belll5145 <Belll@wlhs.wlwv.k12.or.us>
2022-12-13 09:54:38 -0800

    Did contain substring

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,26 @@
 public class Main {
+    /*
+    Use the following methods:
+    .length()
+    .substring(start, finish) doesn't include finish
+    .substring(start) goes to the end
+    .indexOf(someString)
+     someString.equals(otherString)
+     */
     public static void main(String[] args) {
         System.out.println(VowelCount("This is a string."));
         System.out.println("Correct output: 4");
+        System.out.println(VowelCount(""));
+        System.out.println("Correct output: 0");
+        System.out.println(VowelCount("AEIOU"));
+        System.out.println("Correct output: 5");
+        System.out.println(VowelCount("AEI$&*^#!@OU"));
+        System.out.println("Correct output: 5");
         System.out.println(VowelRemover("Remove all of the vowels."));
         System.out.println("Correct output: Rmv ll f th vwls");
+        System.out.println(VowelRemover("Something"));
         System.out.println(ContainsSubstring("Sentence","ten"));
+        System.out.println(ContainsSubstring("bus","cat"));
         System.out.println("Correct output: true");
         System.out.println(ReverseString("ABCDEF"));
         System.out.println("Correct output: FEDCBA");
@@ -18,8 +34,16 @@ public class Main {
      * @return vowel count int
      */
     public static int VowelCount(String input){
-
-        return -1;
+        int length = input.length();
+        String lowerCaseInput = input.toLowerCase();
+        int count = 0;
+        for(int i = 0; i < length; i++){
+            String letter = lowerCaseInput.substring(i,i+1);
+            if(letter.equals("a") || letter.equals("e") || letter.equals("i") || letter.equals("o") || letter.equals("u")){
+                count++;
+            }
+        }
+        return count;
     }
 
     /**
@@ -28,8 +52,25 @@ public class Main {
      * @return String with no vowels
      */
     public static String VowelRemover(String input){
+        /*
+        int variable for the length of the oString
+        String variable = original string
+        make string lowercase
+        use stringVariable.indexOf() to get vowels
 
-        return "";
+         */
+        int length2 = input.length();
+        String lowerCase = input.toLowerCase();
+        String noVowels = "";
+        for(int i2 = 0; i2 < length2; i2++){
+            String letter2 = lowerCase.substring(i2,i2+1);
+            if(!(letter2.toLowerCase().equals("a") || letter2.toLowerCase().equals("e") || letter2.toLowerCase().equals("i")
+            || letter2.toLowerCase().equals("o") || letter2.toLowerCase().equals("u"))){
+                noVowels = noVowels + letter2;
+            }
+        }
+
+        return noVowels;
     }
 
     /**
@@ -39,7 +80,25 @@ public class Main {
      * @return true if target found, false otherwise
      */
     public static boolean ContainsSubstring(String input, String target){
+        /*
+        use .substring()
 
+
+        boolean sub = false;
+        if(input.indexOf(target) >= 0){
+            sub = true;
+        }
+        return sub;
+
+         */
+        /*int length3 = input.length();
+        String lowerCases = input.toLowerCase();
+        for(int i3 = 0; i3 < length3; i3++) {
+
+        }
+        return false;
+
+         */
         return false;
     }
 
